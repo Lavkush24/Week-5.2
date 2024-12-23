ab11200522f0eb127d43a33b4256be0a89c64eae
Lavkush <college.studying23@gmail.com>
2024-12-23 17:33:42 +0530

    resolve the infinite req isssue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,22 @@ import { useState } from 'react'
 import './App.css'
 import { CreateTodo } from './components/Createtodo'
 import { Todos } from './components/Todos'
+import { useEffect } from 'react';
 
 function App() {
 
   const [todos, setTodos] = useState([]);
 
-  setTimeout(() => {
-    fetch("http://localhost:8080/todos")
-      .then(async (res) =>{
-        const todos = await res.json();
-        setTodos(todos);
-      });
-  },1000)
-
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8080/todos")
+        .then(async (res) =>{
+          const todos = await res.json();
+          setTodos(todos);
+        });
+    },5000)
+  });
   
-
   return (
     <div>
       <CreateTodo></CreateTodo>
